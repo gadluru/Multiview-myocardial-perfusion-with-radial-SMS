77a8ac5ed962bd3defd4b7fe26598c64f26bf90d
YeTianMRI <phye1988@gmail.com>
2019-01-08 22:31:25 -0700

    Update Recon_pixel_tracking.m

--- Recon_pixel_tracking.m
+++ Recon_pixel_tracking.m
@@ -7,7 +7,7 @@ para = load_default_para('2D SMS golden NUFFT');
 para.dir.load_kSpace_name = Data_files.name;
 para.dir.load_kSpace_dir = [Data_files.folder,'/'];
 
-para.setting.plot =01;
+para.setting.plot = 0;
 para.setting.ifGPU = 1;
 para.gated = 1;
 
@@ -19,6 +19,6 @@ para.Recon.nor = 30;
 para.image_orintation = [7,3,3];
 para.kSpace_center = 145;
 
-para.slice_pick = 1;
+%para.slice_pick = 1;
 
 [image,para] = Reconstruction_multi_set_SMS_ungated(para);
